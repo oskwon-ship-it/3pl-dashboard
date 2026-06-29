eb52835a8737317ae068cc815c4dd321ca1e3308
oskwon-ship-it <os.kwon@mycommerc.com>
2026-06-29 12:12:35 +0900

    로그인 에러 수정

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
+    config['cookie']['expiry_days']
 )
 
 # Render login widget
